6d0d57915dcfc1072e5c41d864473d77097ba0b4
ggapsang <58983068+ggapsang@users.noreply.github.com>
2023-08-10 23:13:39 +0900

    Update getMdmIDforPiping.py

--- getMdmIDforPiping.py
+++ getMdmIDforPiping.py
@@ -6,105 +6,105 @@ import random
 import time
 import datetime
 
-def genMDMid(ldWs) :
+def gen_MDM_id(load_ws) :
     print("STEP 1st : 해더값 세팅 중...")
 
-    def makingMDMid (ldWs, mdmIdLst, mdmIdLst_rowNo) :
+    def making_MDM_id_conca (load_ws, mdm_id_lst, mdm_id_lst_row_no) :
         print("MDM ID 생성 중...")
-        rowNo = 2
-        v = 1
-        for row in ldWs.iter_rows(min_row=2) :
-            print(v,"의 mdm id 생성")
-            if row[mdmUp_col].value != "X" :
+        row_no = 2
+        #v = 1
+        for row in load_ws.iter_rows(min_row=2) :
+            #print(v,"의 mdm id 생성")
+            if row[mdm_upload_col].value != "X" :
                 naming_codes = []
-                for cell_col in ldHdrNoDict.values() :
-                    if cell_col != mdmId_col and cell_col != mdmUp_col and row[cell_col].value is not None :
+                for cell_col in load_hdr_no_dic.values() :
+                    if cell_col != mdm_id_col and cell_col != mdm_upload_col and row[cell_col].value is not None :
                         naming_codes.append(str(row[cell_col].value))
-                row[mdmId_col].value = '-'.join(naming_codes) # row[key_col].value =mdmID
-                mdmIdLst.append(row[mdmId_col].value)
-                mdmIdLst_rowNo.append(rowNo)
-            rowNo = rowNo + 1
+                row[mdm_id_col].value = '-'.join(naming_codes) # row[key_col].value =mdmID
+                mdm_id_lst.append(row[mdm_id_col].value)
+                mdm_id_lst_row_no.append(row_no)
+            row_no = row_no + 1
             v=v+1
 
-    def checkingDupl(mdmIdDict, indiv_dict) :
+    def checkingDupl(mdm_id_dict, indiv_dict) :
         print("중복 MDM ID 확인 중...")
         seen = []
-        for ky, val in mdmIdDict.items() :
+        for ky, val in mdm_id_dict.items() :
             if val not in seen :
                 seen.append(val)
                 indiv_dict[ky] = val
 
         return indiv_dict
 
-    hdrLst = ["MDM 등록 여부", "MDM 설비 ID", "사용 유체 코드", "태그 시리얼 번호", "배관 사이즈", "배관 사양 코드", "배관 보온 코드", "배관 트레이싱 코드", "배관 자켓 코드"]
+    hdr_lst = ["MDM 등록 여부", "MDM 설비 ID", "사용 유체 코드", "태그 시리얼 번호", "배관 사이즈", "배관 사양 코드", "배관 보온 코드", "배관 트레이싱 코드", "배관 자켓 코드"]
     key = "MDM 설비 ID" 
     key2 = "MDM 등록 여부"
     key3 = "태그 시리얼 번호"
-    ldHdrNoDict = {}
+    load_hdr_no_dic = {}
 
     # print(ldWs[1])
-    for i in ldWs[1] : # 1은 해더가 있는 행의 값
-        if i.value in hdrLst and i.value not in ldHdrNoDict.keys() :
-            ldHdrNoDict[i.value] = i.column - 1
+    for i in load_ws[1] : # 1은 해더가 있는 행의 값
+        if i.value in hdr_lst and i.value not in load_hdr_no_dic.keys() :
+            load_hdr_no_dic[i.value] = i.column - 1
 
-    mdmId_col = ldHdrNoDict.get(key)
-    mdmUp_col = ldHdrNoDict.get(key2)
-    srNo_col = ldHdrNoDict.get(key3)
+    mdm_id_col = load_hdr_no_dic.get(key)
+    mdm_upload_col = load_hdr_no_dic.get(key2)
+    sr_no_col = load_hdr_no_dic.get(key3)
 
 
     print("2nd STEP : 1차 MDM ID 생성 중...")
-    mdmIdLst = []
-    mdmIdLst_rowNo = []
-    makingMDMid(ldWs=ldWs, mdmIdLst=mdmIdLst, mdmIdLst_rowNo=mdmIdLst_rowNo)
-    mdmIdDict = dict(zip(mdmIdLst_rowNo, mdmIdLst))
+    mdm_id_lst = []
+    mdm_id_lst_row_no = []
+    making_MDM_id_conca(load_ws=load_ws, mdm_id_lst=mdm_id_lst, mdm_id_lst_row_no=mdm_id_lst_row_no)
+    mdm_id_dic = dict(zip(mdm_id_lst_row_no, mdm_id_lst))
 
 
     print("3rd STEP : 중복 체크 리스트 생성 중...")
     checklst = []
     indiv_dict = {}
-    checkingDupl(mdmIdDict, indiv_dict)
-    checklst = [x for x in mdmIdDict.keys() if x not in indiv_dict.keys()]
+    checkingDupl(mdm_id_dic, indiv_dict)
+    checklst = [x for x in mdm_id_dic.keys() if x not in indiv_dict.keys()]
     print("mdm id 중복 행 : ", checklst)
     print("mdm id 중복 개수: ", len(checklst))
 
 
     print("4th STEP : 중복 MDM ID 변경 중...")
-    rowNo = 2
+    row_no = 2
     while len(checklst) > 0 :
-        for rowNo in checklst :
-            for row in ldWs.iter_rows(min_row=rowNo, max_row=rowNo) :
+        for row_no in checklst :
+            for row in load_ws.iter_rows(min_row=row_no, max_row=row_no) :
                 # 중복 mdm id를 갖는 값의 serial no 변경
-                srNoSplit = list(str(row[srNo_col].value))
-                newSrNo_lst = []
-                for i in srNoSplit :
+                sr_no_split = list(str(row[sr_no_col].value))
+                new_sr_no_lst = []
+                for i in sr_no_split :
                     try :
                         i = int(i)
                         i = random.randrange(0, 10)
-                        newSrNo_lst.append(str(i))
+                        new_sr_no_lst.append(str(i))
                     except :
-                        newSrNo_lst.append(str(i))
-                row[srNo_col].value = ''.join(newSrNo_lst)
+                        new_sr_no_lst.append(str(i))
+                row[sr_no_col].value = ''.join(new_sr_no_lst)
                 # print(row[srNo_col].value)
 
                 # 변경된 serial no 칼라마킹
-                ldWs.cell(rowNo, srNo_col+1).fill = PatternFill(fill_type='solid', fgColor=Color('FFFF00'))
+                load_ws.cell(row_no, sr_no_col+1).fill = PatternFill(fill_type='solid', fgColor=Color('FFFF00'))
 
                 # 변경된 serial no로 새 mdm id 생성
                 naming_codes = []
-                for cell_col in ldHdrNoDict.values() :
-                    if cell_col != mdmId_col and cell_col != mdmUp_col and row[cell_col].value is not None :
+                for cell_col in load_hdr_no_dic.values() :
+                    if cell_col != mdm_id_col and cell_col != mdm_upload_col and row[cell_col].value is not None :
                         naming_codes.append(str(row[cell_col].value))
-                row[mdmId_col].value = '-'.join(naming_codes) # row[key_col].value =mdmID
+                row[mdm_id_col].value = '-'.join(naming_codes) # row[key_col].value =mdmID
                 # print(row[mdmId_col].value)
 
                 # mdm id 딕셔너리에 새로 반영함
-                mdmIdDict[rowNo] = row[mdmId_col].value
+                mdm_id_dic[row_no] = row[mdm_id_col].value
 
         # 중복 체크 루프
         checklst = []
         indiv_dict = {}
-        checkingDupl(mdmIdDict, indiv_dict)
-        checklst = [x for x in mdmIdDict.keys() if x not in indiv_dict.keys()]
+        checkingDupl(mdm_id_dic, indiv_dict)
+        checklst = [x for x in mdm_id_dic.keys() if x not in indiv_dict.keys()]
         print(len(checklst))
     print("루프문 종료. 중복 태그 없음")
 
@@ -112,16 +112,16 @@ start = time.time()
 
 print("1st STEP : 파일 불러오는 중..")
 path = "C:/Users/dayoung.kweon/Documents/00_Python/"
-loadFilename = input("파일이름 : ") + ".xlsx"
-loadWb = openpyxl.load_workbook(path + loadFilename)
+load_file_name = input("파일이름 : ") + ".xlsx"
+load_wb = openpyxl.load_workbook(path + load_file_name)
 # loadWb.save(path + "백업파일/" + loadFilename)
 
 for x in ["배관_BD2", "배관_BRU"] :
-    ldWs = loadWb[x]
-    genMDMid(ldWs)
+    load_ws = load_wb[x]
+    gen_MDM_id(load_ws)
 
 print("저장 중...")
-loadWb.save(path + loadFilename)
+load_wb.save(path + load_file_name)
 
 end = time.time()
 sec = end - start
